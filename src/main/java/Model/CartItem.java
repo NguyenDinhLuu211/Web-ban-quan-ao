7b682ab1ff1a38c58108c54cd36902d1c1385709
Pham-Hai-Duong <146476616+Pham-Hai-Duong@users.noreply.github.com>
2024-05-28 22:47:43 +0700

    CartItem.java

--- src/main/java/Model/CartItem.java
+++ src/main/java/Model/CartItem.java
@@ -3,7 +3,58 @@ package Model;
 public class CartItem {
     private Product product;
     private int quantity;
+    //
+	private int accountID;
+	private int productID;
+	private int maCart;
+	private String size;
+	
+	
 
+	public CartItem(int quantity, int accountID, int productID, int maCart, String size) {
+		super();
+		this.quantity = quantity;
+		this.accountID = accountID;
+		this.productID = productID;
+		this.maCart = maCart;
+		this.size = size;
+	}
+
+	public CartItem() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getAccountID() {
+		return accountID;
+	}
+
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+
+	public int getMaCart() {
+		return maCart;
+	}
+
+	public void setMaCart(int maCart) {
+		this.maCart = maCart;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public int getProductID() {
+		return productID;
+	}
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
 
 
     public Product getProduct() {
